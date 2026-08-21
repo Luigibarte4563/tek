e36b2d604dd09ff81da09576d4ac9e3435b30609
Luigi Barte <luigibarte1@gmail.com>
2026-08-21 21:47:09 +0800

    Add Main class to greet user with name and age

---
+++ concat.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Luigi";
+        int age = 18;
+
+        System.out.println("Hello, " + name + "!");
+        System.out.println("You are " + age + " years old.");
+    }
+}
